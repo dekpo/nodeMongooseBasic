ee79472ae95d4f139e7cfd385b618fde2e14a691
Dekpo Yologaza <dekpo@me.com>
2021-03-17 15:01:45 +0100

    Added Express and some end points to display JSON data

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // on importe le module mongoose
 const mongoose = require('mongoose');
+// on importe Express pour servir les résultats en http
+const express = require('express');
+// on utilise Express sur le port 3000
+const app = express();
+app.listen(3000);
+app.get('/', ( request , response ) => {
+    response.send('Hello World! The Express Server IS RUNNING !!!');
+  });
+
 // on définit la constante uri qui est l'adresse de notre serveur MongoDB
 const URI = "mongodb+srv://dekpo:qi08xn6@cluster0.dgrcq.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 // on se connecte à notre serveur MongoDB
@@ -17,7 +26,12 @@ count.exec((err,result)=>{
     console.log('Result Count:', result);
 });
 // on effetue une autre recherche avec find et les méthodes connues de MongoDB
-const listing = myModel.find({},{name:1}).limit(5);
-listing.exec( (err, result) => {
-    console.log( result );
+const listing = myModel.find({name:/a/},{_id:1,name:1,listing_url:1}).skip(10).limit(20).sort({_id:1});
+// on affiche les résultats de la requete Mongo sur le serveur Express
+// à l'adresse /api
+app.get('/api', ( request , response ) => {
+    listing.exec( (err, result) => {
+        response.send( result );
+        console.log( result );
+    });
 });
